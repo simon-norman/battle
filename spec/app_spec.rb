949532ae7c5ced7d5e88b6896537b8b5f5170aa0
Luke Phyall <lukeprospect@yahoo.com>
2019-02-20 15:14:52 +0000

    Adds player 2 hit points

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,16 +1,29 @@
 require 'capybara/rspec'
 
-feature 'Testing infrastructure' do
-  scenario 'Can input user names, submit, then see names on screen' do
-    visit('/')
-    player_1_name = 'Simon'
-    player_2_name = 'Luke'
+feature "Testing infrastructure" do
 
-    fill_in 'player_1_name', with: player_1_name
-    fill_in 'player_2_name', with: player_2_name
+  feature "When user inputs player names and submits" do
 
-    click_button 'submit'
+    let(:player_1_name){ "Simon"}
+    let(:player_2_name){ "Luke"}
 
-    expect(page).to have_content player_1_name && player_2_name
+    before(:each) do
+
+      visit('/')
+      fill_in 'player_1_name', with: player_1_name
+      fill_in 'player_2_name', with: player_2_name
+      click_button 'submit'
+
+    end
+
+    scenario 'Can input user names, submit, then see names on screen' do
+      expect(page).to have_content player_1_name && player_2_name
+    end
+
+    scenario "Be able to see player 2's hit points" do
+      expect(page).to have_content "Player 2's hit points: 20"
+    end
+
   end
+
 end
