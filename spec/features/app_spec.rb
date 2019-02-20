7ced0984cfc809e1b4c43758bdf525c62c791637
Simon <01secn@gmail.com>
2019-02-20 15:45:16 +0000

    Moved test set up for inputting player names into helper file

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -1,29 +1,25 @@
 require 'capybara/rspec'
+require_relative 'web_helpers.rb'
 
 feature "Testing infrastructure" do
 
   feature "When user inputs player names and submits" do
 
-    let(:player_1_name){ "Simon"}
-    let(:player_2_name){ "Luke"}
+    let(:player_1_name) { "Simon" }
+    let(:player_2_name) { "Luke" }
 
     before(:each) do
-
-      visit('/')
-      fill_in 'player_1_name', with: player_1_name
-      fill_in 'player_2_name', with: player_2_name
-      click_button 'submit'
-
+      sign_in_and_play(player_1_name, player_2_name)
     end
 
     scenario 'Can input user names, submit, then see names on screen' do
-      expect(page).to have_content player_1_name && player_2_name
+      expect(page).to have_content player_1_name
+      expect(page).to have_content player_2_name
     end
 
     scenario "Be able to see player 2's hit points" do
       expect(page).to have_content "Player 2's hit points: 20"
     end
-
   end
 
 end
